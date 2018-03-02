62999717f468184fc106ef13616a50d4feccea91
Jason <jasonphoon@gmail.com>
2018-03-02 18:55:36 +0800

    'use strict' in main.js

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -29137,7 +29137,7 @@ var About = React.createClass({displayName: "About",
                     )
                 )
             )
-        )
+        );
     }
 });
 
@@ -29167,28 +29167,32 @@ var React = require('react');
 var Home = require('./components/homePage');
 var About = require('./components/about/aboutPage');
 
-var App = React.createClass({displayName: "App",
-    render: function() {
-        var Child;  //which child we want to render (ex: Home or About)
+(function(win) {
+    "use strict";
+    var App = React.createClass({displayName: "App",
+        render: function() {
+            var Child;  //which child we want to render (ex: Home or About)
 
-        switch(this.props.route) {
-            case 'about': Child = About; break;
-            default: Child = Home;
+            switch(this.props.route) {
+                case 'about': Child = About; break;
+                default: Child = Home;
+            }
+
+            return (
+                    React.createElement("div", null, 
+                        React.createElement(Child, null)
+                    )
+            );
         }
+    });
 
-        return (
-                React.createElement("div", null, 
-                    React.createElement(Child, null)
-                )
-        );
+    function render() {
+        var route = win.location.hash.substr(1);
+        React.render(React.createElement(App, {route: route}), document.getElementById('app'));
     }
-});
 
-function render() {
-    var route = window.location.hash.substr(1);
-    React.render(React.createElement(App, {route: route}), document.getElementById('app'));
-}
+    win.addEventListener('haschange', render);
+    render();
 
-window.addEventListener('haschange', render);
-render();
+})(window);
 },{"./components/about/aboutPage":158,"./components/homePage":159,"jquery":1,"react":157}]},{},[160]);
--- src/main.js
+++ src/main.js
@@ -3,27 +3,31 @@ var React = require('react');
 var Home = require('./components/homePage');
 var About = require('./components/about/aboutPage');
 
-var App = React.createClass({
-    render: function() {
-        var Child;  //which child we want to render (ex: Home or About)
+(function(win) {
+    "use strict";
+    var App = React.createClass({
+        render: function() {
+            var Child;  //which child we want to render (ex: Home or About)
 
-        switch(this.props.route) {
-            case 'about': Child = About; break;
-            default: Child = Home;
+            switch(this.props.route) {
+                case 'about': Child = About; break;
+                default: Child = Home;
+            }
+
+            return (
+                    <div>
+                        <Child />
+                    </div>
+            );
         }
+    });
 
-        return (
-                <div>
-                    <Child />
-                </div>
-        );
+    function render() {
+        var route = win.location.hash.substr(1);
+        React.render(<App route={route} />, document.getElementById('app'));
     }
-});
 
-function render() {
-    var route = window.location.hash.substr(1);
-    React.render(<App route={route} />, document.getElementById('app'));
-}
+    win.addEventListener('haschange', render);
+    render();
 
-window.addEventListener('haschange', render);
-render();
+})(window);
